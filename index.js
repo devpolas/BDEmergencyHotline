ee057d9b91a235142aa5c42e7ccdb4cdf3f07ec3
devpolas <195308698+devpolas@users.noreply.github.com>
2025-08-27 11:47:06 +0600

    implement functionality for heart with inc and dec

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+// update value by inc or dec with id
+function updateValue(count, id) {
+  if (count === "inc") {
+    const element = document.getElementById(id);
+    const elementValue = element.innerText;
+    if (Number(elementValue) < 0) {
+      return;
+    }
+    element.innerText = Number(elementValue) + 1;
+    return;
+  }
+  if (count === "dec") {
+    const element = document.getElementById(id);
+    const elementValue = element.innerText;
+    if (Number(elementValue) < 0) {
+      return;
+    }
+    element.innerText = Number(elementValue) - 1;
+    return;
+  }
+}
 
+// Handel all heart button and update value
+const allHeart = document.querySelectorAll(".heart");
+for (const heart of allHeart) {
+  heart.addEventListener("click", function () {
+    heart.classList.toggle("text-gray-300");
+    heart.classList.toggle("text-red-600");
+    if (heart.classList.contains("text-gray-300")) {
+      updateValue("dec", "love");
+    } else if (heart.classList.contains("text-red-600")) {
+      updateValue("inc", "love");
+    }
+  });
+}
